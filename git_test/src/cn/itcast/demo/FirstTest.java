cce3acddbe58ef4ec0a9da86113b6fb503c34c84
hanx <hanx0123@163.com>
2019-03-07 10:51:01 +0800

    将idea中的工程添加到本地仓库

---
+++ git_test/src/cn/itcast/demo/FirstTest.java
@@ -0,0 +1,14 @@
+package cn.itcast.demo;
+
+/**
+ * @Author:
+ * @Date:2019/3/7 10:31
+ * @Description:
+ */
+public class FirstTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
